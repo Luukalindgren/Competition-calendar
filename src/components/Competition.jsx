384ed980104d1cd7d079119607bc9b4bf709be2e
Luuka Lindgren <luslin@utu.fi>
2023-04-22 14:59:28 +0300

    Star icon for marking favourites

--- src/components/Competition.jsx
+++ src/components/Competition.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { StarOutlined, StarFilled } from "@ant-design/icons"
+import "../App.css"; 
+
+// Add star icon to the competition for marking it to favourites
 
 export default function Competition(props) {
   const weatherForecast =
@@ -8,6 +12,7 @@ export default function Competition(props) {
   return (
     <li className="Main-list-item">
       <h2>{props.name}</h2>
+      <div className="Favourite-icon"><StarOutlined></StarOutlined></div>
       <p>{props.location}</p>
       <b>{props.time}</b>
       <p>{props.area}</p>
