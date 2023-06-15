f7397e3c3fe51d01098496a8688285b2a2b1960e
Luuka Lindgren <luslin@utu.fi>
2023-06-15 11:03:16 +0300

    Added data patching (marking favourite)

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Competition = require("./models/competition");
-const cron = require("node-cron");
 require("dotenv").config();
 
 // TODO:
@@ -12,11 +11,25 @@ require("dotenv").config();
 // DONE Change React to use API/DB instead of static data "http://localhost:3000/competitions"
 // DONE Hide MongoDB connection string to .env file
 // HALF DONE Add the functionalities in README.md
-// DONE Publish frontend to GitHub Pages
+// Done Publish frontend to GitHub Pages
 // DONE Publish backend to Heroku or AWS or something
-// - FIX render.com that backend runs scraper
+// - FIX the scraper that it works on render.com
 // - Add a area selector that shows the narrows down the competitions to the selected area
 //
+
+// Update the database every render
+console.log("Running scraper");
+const { spawn } = require("child_process");
+const pythonProcess = spawn("python", ["./scraper/scraper.py"]);
+pythonProcess.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+pythonProcess.stderr.on("data", (data) => {
+  console.log(data.toString());
+});
+pythonProcess.on("close", (code) => {
+  console.log(`Python scraper process exited with code ${code}`);
+});
 
 const URL = process.env.DATABASE_URL;
 
@@ -33,22 +46,27 @@ app.get("/competitions", async (req, res) => {
   res.json(competitions);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", async (req, res) => {
+  res.status(301).redirect("/competitions");
 });
 
-// Update the database every hour
-cron.schedule("0 * * * *", () => {
-  console.log("Running scraper every 30 min");
-  const { spawn } = require("child_process");
-  const pythonProcess = spawn("python", ["./scraper/scraper.py"]);
-  pythonProcess.stdout.on("data", (data) => {
-    console.log(data.toString());
-  });
-  pythonProcess.stderr.on("data", (data) => {
-    console.log(data.toString());
-  });
-  pythonProcess.on("close", (code) => {
-    console.log(`Python scraper process exited with code ${code}`);
-  });
+app.patch("/competitions/:id", async (req, res) => {
+  try {
+    const competitionId = req.params.id;
+    const { favourite } = req.body;
+
+    const competition = await Competition.findByIdAndUpdate(
+      competitionId,
+      { favourite },
+      { new: true }
+    ).exec();
+    res.json(competition);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
